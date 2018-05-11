c93dbd9810f9931006a5753a99df56f2586343db
Michael Dillie <michaeldillie@gmail.com>
2018-05-11 09:20:57 -0500

    Finding a stopping ponit

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.IyOvR1LEh6p48xXwnGKpz93CN,
+  consumer_secret: process.env.GiklZTLbc8GNHc5TVORngDH6sDOgQQZELDFKovTHfeaxmDrro5,
+  access_token_key: process.env.mgzqw90W6tykjv5SdolESwRxv1Y6zH0,
+  access_token_secret: process.env.XGFPrr330k5wq6mZpPR9FFCHgOGVG9oIIPPwnws00xHrw
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
