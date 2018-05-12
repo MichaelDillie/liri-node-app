6fa5dd54871cb6289624934d4abfc56c90424087
Michael Dillie <michaeldillie@gmail.com>
2018-05-11 21:16:51 -0500

    Everthing is set up to build on

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.IyOvR1LEh6p48xXwnGKpz93CN,
-  consumer_secret: process.env.GiklZTLbc8GNHc5TVORngDH6sDOgQQZELDFKovTHfeaxmDrro5,
-  access_token_key: process.env.mgzqw90W6tykjv5SdolESwRxv1Y6zH0,
-  access_token_secret: process.env.XGFPrr330k5wq6mZpPR9FFCHgOGVG9oIIPPwnws00xHrw
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
