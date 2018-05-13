50b243820a523bca6e93c6180938e96eb2748c9d
Michael Dillie <michaeldillie@gmail.com>
2018-05-12 22:47:05 -0500

    Twitter works. Huge issue with a node file being 'Removed From Disk' but crisis averted

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+var dotenv = require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
 var request = require('request');
@@ -17,6 +17,21 @@ var movieName = "";
 // Twitter
 if(command === "my-tweets") {
     var client = new Twitter(keys.twitter);
+    var params = { screen_name: "myfaketwit2", count: 10 };
+
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+
+      if (!error) {
+        var data = []; //empty array to hold data
+        for (var i = 0; i < tweets.length; i++) {
+          data.push({
+              'created at: ' : tweets[i].created_at,
+              'Tweets: ' : tweets[i].text,
+          });
+        }
+        console.log(data);
+      }
+    });
 
 }
 
