130e34bffd107057027c366361579a2a4414fb03
Michael Dillie <michaeldillie@gmail.com>
2018-05-14 08:31:57 -0500

    Twitter, Spotify, and movie are working

--- liri.js
+++ liri.js
@@ -6,55 +6,72 @@ var request = require('request');
 // Importing keys
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
-
 var nodeArgs = process.argv;
 var command = process.argv[2];
 
 var movieName = "";
+var songName = "";
+var twitterName = "";
 
 
 // Twitter
-if(command === "my-tweets") {
+if (command === "my-tweets") {
     var client = new Twitter(keys.twitter);
-    var params = { screen_name: "myfaketwit2", count: 10 };
+    for (var i = 3; i < nodeArgs.length; i++) {
+        twitterName += nodeArgs[i] + " ";
+    }
 
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    var params = {
+        screen_name: twitterName,
+        count: 10
+    };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
 
-      if (!error) {
-        var data = []; //empty array to hold data
-        for (var i = 0; i < tweets.length; i++) {
-          data.push({
-              'created at: ' : tweets[i].created_at,
-              'Tweets: ' : tweets[i].text,
-          });
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log("----------------------------------------------------------------------");
+                console.log("Created at: " + tweets[i].created_at);
+                console.log("Tweet: " + tweets[i].text)
+                console.log("----------------------------------------------------------------------");
+            }
         }
-        console.log(data);
-      }
     });
-
 }
 
 // Spotify
-else if(command === "spotify-this-song") {
-    spotify.search({ type: 'track', query: 'All the Small Things' })
-    .then(function(response) {
-      console.log(response);
-    })
-    .catch(function(err) {
-      console.log(err);
+else if (command === "spotify-this-song") {
+    var spotify = new Spotify(keys.spotify);
+    for (var i = 3; i < nodeArgs.length; i++) {
+        songName += nodeArgs[i] + " ";
+    }
+
+    spotify.search({
+        type: 'track',
+        query: songName
+    }, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        } else {
+                console.log("----------------------------------------------------------------------");
+                console.log("Song Name: " + songName.toUpperCase());
+                console.log("Album Name: " + data.tracks.items[0].album.name);
+                console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name);
+                console.log("URL: " + data.tracks.items[0].album.external_urls.spotify);
+                console.log("----------------------------------------------------------------------");
+        }
     });
 }
 
 // OMDB
-else if(command === "movie-this") {
-    for(var i = 3; i < nodeArgs.length; i++) {
+else if (command === "movie-this") {
+    for (var i = 3; i < nodeArgs.length; i++) {
         movieName += nodeArgs[i] + " ";
     }
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-    request(queryUrl, function(error, response, body) {
-        if(!error && response.statusCode === 200) {
+    request(queryUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
             console.log("-----------------------------------------------------------------------");
             console.log("Title: " + JSON.parse(body).Title);
             console.log("Year: " + JSON.parse(body).Year);
@@ -71,9 +88,8 @@ else if(command === "movie-this") {
 }
 
 // Do What It Says
-
-else if(command === "do-what-it-says") {
-    for(var i = 3; i < nodeArgs.length; i++) {
+else if (command === "do-what-it-says") {
+    for (var i = 3; i < nodeArgs.length; i++) {
         console.log(nodeArgs[i]);
     }
 }
